6e79a9ea1033a7223804555c91ee508bd98b298e
sebagomez <sebagomezcorrea@outlook.com>
2017-03-29 10:13:42 -0300

    Length of Last word added

---
+++ src/LengthOfLastWord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class LengthOfLastWord
+	{
+		public static int FindLengthOfLastWord(string s)
+		{
+			string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			if (words.Length > 0)
+				return words[words.Length - 1].Length;
+
+			return 0;
+		}
+	}
+}
---
+++ tests/UnitTestProject1/LengthOfLastWordTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class LengthOfLastWordTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			InternalTest("Hello World", 5);
+		}
+
+		void InternalTest(string line, int expected)
+		{
+			int value = LengthOfLastWord.FindLengthOfLastWord(line);
+			Assert.AreEqual<int>(value, expected, $"The last word of {line} returned {value}, and {expected} was expected");
+		}
+	}
+}
