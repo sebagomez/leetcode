defda2b1071bad01b5aa108cfa93b3161db04107
sebagomez <sebagomezcorrea@outlook.com>
2017-03-29 10:06:01 -0300

    BuyAndSellsStocks added

---
+++ src/BuyAndSellStocks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class BuyAndSellStocks
+	{
+		public static int MaxProfit(int[] prices)
+		{
+			int sell = 0, prev_sell = 0, buy = int.MinValue, prev_buy;
+			foreach (int price in prices)
+			{
+				prev_buy = buy;
+				buy = Math.Max(prev_sell - price, prev_buy);
+				prev_sell = sell;
+				sell = Math.Max(prev_buy + price, prev_sell);
+			}
+			return sell;
+		}
+	}
+}
---
+++ tests/UnitTestProject1/BuyAndSellStocksTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class BuyAndSellStocksTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			InternalTest(new int[] { 1, 2, 3, 0, 2 }, 3);
+
+		}
+
+		void InternalTest(int[] prices, int expected)
+		{
+			int value = BuyAndSellStocks.MaxProfit(prices);
+			Assert.AreEqual<int>(value, expected, $"For {prices} returned {value}");
+		}
+	}
+}
