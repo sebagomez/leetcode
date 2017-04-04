da1020191abec5626f0ccf82019c44ffdf56b458
sebagomez <sebagomezcorrea@outlook.com>
2017-04-04 10:29:45 -0300

    Container With Most Water

---
+++ src/ContainerWithMostWater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode
+{
+	public class ContainerWithMostWater
+	{
+		public static int MaxArea(int[] height)
+		{
+			int max = int.MinValue;
+			for (int i = 0; i < height.Length; i++)
+			{
+				for (int j = i + 1; j < height.Length; j++)
+				{
+					max = Math.Max(max, (j - i) * Math.Min(height[i], height[j]));
+				}
+			}
+
+			return max;
+		}
+	}
+}
---
+++ tests/UnitTestProject1/ContainerWithMostWaterTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class ContainerWithMostWaterTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			InternalTest(new int[] { 1, 1 }, 1);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			InternalTest(new int[] { 0, 2 }, 0);
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			InternalTest(new int[] { 1, 2, 1 }, 2);
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			InternalTest(new int[] { 1, 2, 4, 3 }, 4);
+		}
+
+		void InternalTest(int[] height, int expected)
+		{
+			int value = ContainerWithMostWater.MaxArea(height);
+			Assert.AreEqual<int>(expected, value, $"Expected: {expected}, got {value}");
+		}
+	}
+}
