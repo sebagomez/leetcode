e95ae84aaf25036aea95d28232c576f3844792c2
seba gomez <sebagomezcorrea@outlook.com>
2019-05-28 19:40:43 -0300

    MergeKSortedList [hard]

---
+++ src/MergeKSortedList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class MergeKSortedList
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x) { val = x; }
+
+			public ListNode(string list)
+			{
+				string[] values = list.Split(new char[] { ',' });
+				ListNode last = null;
+				foreach (string v in values)
+				{
+					if (last == null)
+					{
+						val = int.Parse(v);
+						last = this;
+					}
+					else
+					{
+						ListNode child = new ListNode(int.Parse(v));
+						last.next = child;
+						last = child;
+					}
+				}
+			}
+
+			public override string ToString()
+			{
+				return $"{val}{(next != null ? "->" + next.ToString() : string.Empty)}";
+			}
+		}
+
+		public static ListNode MergeKLists(ListNode[] lists)
+		{
+			Dictionary<int, ListNode> index = new Dictionary<int, ListNode>();
+			for (int i = 0; i < lists.Length; i++)
+			{
+				index[i] = lists[i];
+			}
+
+			int min = int.MaxValue;
+			ListNode root = null;
+			ListNode last = null;
+
+			while (index.Any( kv => kv.Value != null))
+			{
+				foreach (int i in index.Keys)
+				{
+					if(index[i] != null)
+						min = Math.Min(min, index[i].val);
+				}
+
+				foreach (int i in index.Keys)
+				{
+					if (index[i] != null && index[i].val == min)
+					{
+						index[i] = index[i].next;
+						break;
+					}
+				}
+
+				if (root == null)
+				{
+					root = new ListNode(min);
+					last = root;
+				}
+				else
+				{
+					ListNode child = new ListNode(min);
+					last.next = child;
+					last = child;
+				}
+				min = int.MaxValue;
+			}
+
+			return root;
+		}
+	}
+}
---
+++ tests/UnitTestProject1/MergeKSortedListTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static LeetCode.MergeKSortedList;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class MergeKSortedListTests
+	{
+		[TestMethod]
+		public void TestCase1()
+		{
+			List<ListNode> lists = new List<ListNode>
+			{
+				new ListNode("1,4,5"),
+				new ListNode("1,3,4"),
+				new ListNode("2,6")
+			};
+
+			InternalTest(lists.ToArray(), "1->1->2->3->4->4->5->6");
+
+		}
+
+		void InternalTest(ListNode[] lists, string expected)
+		{
+			ListNode result = MergeKSortedList.MergeKLists(lists);
+			Assert.AreEqual(expected, result.ToString());
+		}
+	}
+}
