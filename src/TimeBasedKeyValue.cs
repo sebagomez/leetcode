0d04a3092b54cceca24c3957d1a99e3a00050c34
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-09-14 16:38:50 -0300

    new algos

---
+++ src/TimeBasedKeyValue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	/// <summary>
+	/// https://leetcode.com/problems/time-based-key-value-store/
+	/// </summary>
+	public class TimeBasedKeyValue
+	{
+		public class TimeMap
+		{
+			public class TimeValue
+			{
+				public string Value { get; set; }
+				public int Timestamp { get; set; }
+				public TimeValue(string value, int timestamp)
+				{
+					Value = value;
+					Timestamp = timestamp;
+				}
+			}
+
+			Dictionary<string, List<TimeValue>> values;
+			/** Initialize your data structure here. */
+			public TimeMap()
+			{
+				values = new Dictionary<string, List<TimeValue>>();
+			}
+
+			public void Set(string key, string value, int timestamp)
+			{
+				if (!values.ContainsKey(key))
+				{
+					values[key] = new List<TimeValue> { new TimeValue(value, timestamp) };
+				}
+				else
+				{
+					values[key].Add(new TimeValue(value, timestamp));
+				}
+			}
+
+			public string Get(string key, int timestamp)
+			{
+				if (!values.ContainsKey(key))
+					return "";
+
+				List<TimeValue> list = values[key];
+
+
+				int start = 0;
+				int end = list.Count - 1;
+				if (timestamp < list[start].Timestamp)
+					return "";
+
+				if (timestamp > list[end].Timestamp)
+					return list[end].Value;
+				
+				int mid = 0;
+				int currTimestamp = 0;
+				while (start <= end)
+				{
+					mid = (start + end) / 2;
+					currTimestamp = list[mid].Timestamp;
+					if (currTimestamp == timestamp)
+					{
+						return list[mid].Value;
+					}
+					else if (timestamp < currTimestamp)
+					{
+						end = mid - 1;
+					}
+					else if (timestamp > currTimestamp)
+					{
+						start = mid + 1;
+					}
+				}
+				return list[end].Timestamp > list[start].Timestamp ? list[start].Value : list[end].Value;
+			}
+		}
+	}
+}
---
+++ src/TrappingRainWater.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class TrappingRainWater
+	{
+		public int Trap(int[] height)
+		{
+			int max = GetMaxHeight(height);
+			int floorWater = 0;
+			for (int f = 1; f <= max; f++) //floors
+			{
+				bool start = false;
+				int bucketWater = 0;
+				for (int i = 0; i < height.Length; i++) // holes
+				{
+					if (!start && height[i] < f)
+						continue;
+
+					start = true;
+
+					if (height[i] < f)
+					{
+						if (i != height.Length - 1)
+						{
+							bucketWater++;
+						}
+					}
+					else
+					{
+						floorWater += bucketWater;
+						bucketWater = 0;
+					}
+				}
+			}
+
+			return floorWater;
+		}
+
+		private static int GetMaxHeight(int[] height)
+		{
+			if (height.Length == 0)
+				return 0;
+
+			int max = int.MinValue;
+			for (int i = 0; i < height.Length; i++)
+			{
+				max = Math.Max(max, height[i]);
+			}
+
+			return max;
+		}
+
+		public static int SmartTrap(int[] height)
+		{
+			if (height.Length == 0)
+				return 0;
+
+			int left = 0, right = height.Length - 1;
+			int leftMax = 0, rightMax = 0;
+			int ans = 0;
+			while (left < right)
+			{
+				if (height[left] > leftMax)
+					leftMax = height[left];
+
+				if (height[right] > rightMax)
+					rightMax = height[right];
+
+				if (leftMax < rightMax)
+				{
+					ans += Math.Max(0, leftMax - height[left]);
+					left++;
+				}
+				else
+				{
+					ans += Math.Max(0, rightMax - height[right]);
+					right--;
+				}
+			}
+			return ans;
+		}
+	}
+}
