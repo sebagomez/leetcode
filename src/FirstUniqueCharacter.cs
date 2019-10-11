8f019da88ba86bc9394dd61f097d8db28e219598
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-11 20:03:38 -0300

    more string problems

---
+++ src/FirstUniqueCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class FirstUniqueCharacter
+	{
+		public static int FirstUniqChar(string s)
+		{
+			Dictionary<char, List<int>> counts = new Dictionary<char, List<int>>();
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (counts.ContainsKey(s[i]))
+					counts[s[i]].Add(i);
+				else
+					counts[s[i]] = new List<int>() { i };
+			}
+
+			foreach (var key in counts.Keys)
+			{
+				if (counts[key].Count == 1)
+					return counts[key][0];
+			}
+
+			return - 1;
+		}
+	}
+}
---
+++ src/ValidAnagram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class ValidAnagram
+	{
+		public static bool IsAnagram(string s, string t)
+		{
+			if (s.Length != t.Length)
+				return false;
+
+			Dictionary<char, int> aux = new Dictionary<char, int>();
+			for (int i = 0; i < t.Length; i++)
+			{
+				if (aux.ContainsKey(t[i]))
+					aux[t[i]]++;
+				else
+					aux[t[i]] = 1;
+			}
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (!aux.ContainsKey(s[i]))
+					return false;
+				
+				aux[s[i]]--;
+				if (aux[s[i]] == 0)
+					aux.Remove(s[i]);
+			}
+
+			return aux.Count == 0;
+		}
+	}
+}
