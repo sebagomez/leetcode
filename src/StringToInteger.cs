89b444885033a206a0dc3910f618bcdfbe35d467
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-12 11:43:55 -0300

    atoi implemented

---
+++ src/StringToInteger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class StringToInteger
+	{
+		public static int MyAtoi(string str)
+		{
+			str = str.Trim();
+			if (string.IsNullOrEmpty(str))
+				return 0;
+
+			string strValue = "";
+			bool negative = false;
+			for (int i = 0; i < str.Length; i++)
+			{
+				char c = str[i];
+				if (i == 0)
+				{
+					if (c == '-')
+					{
+						negative = true;
+						continue;
+					}
+					else if (c == '+')
+					{
+						negative = false;
+						continue;
+					}
+				}
+
+				if (c >= 48 && c <= 57)
+					strValue += c;
+				else
+					break;
+			}
+
+			if (string.IsNullOrEmpty(strValue))
+				return 0;
+
+			try
+			{
+				return negative ? int.Parse("-" + strValue) : int.Parse(strValue);
+			}
+			catch (OverflowException)
+			{
+				if (negative)
+					return int.MinValue;
+				else
+					return int.MaxValue;
+			}
+		}
+	}
+}
---
+++ tests/UnitTestProject1/StringToIntegerTests.cs
@@ -0,0 +1,56 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class StringToIntegerTests
+	{
+		[TestMethod]
+		public void StringToIntegerTests1()
+		{
+			InternalTest("42",42);
+		}
+
+		[TestMethod]
+		public void StringToIntegerTests2()
+		{
+			InternalTest("   -42", -42);
+		}
+
+		[TestMethod]
+		public void StringToIntegerTests3()
+		{
+			InternalTest("4193 with words", 4193);
+		}
+
+		[TestMethod]
+		public void StringToIntegerTests4()
+		{
+			InternalTest("-91283472332", int.MinValue);
+		}
+
+		[TestMethod]
+		public void StringToIntegerTests5()
+		{
+			InternalTest("+1", 1);
+		}
+
+		[TestMethod]
+		public void StringToIntegerTests6()
+		{
+			InternalTest(" + 0 123", 0);
+		}
+
+		void InternalTest(string str, int expected)
+		{
+			int actual = StringToInteger.MyAtoi(str);
+			Assert.AreEqual<int>(expected, actual);
+		}
+	}
+}
