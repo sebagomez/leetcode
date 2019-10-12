91d7ab39997f5f5b3dc4ed62b620d771d097c2eb
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-12 14:52:21 -0300

    StrStr

---
+++ src/ImplementStrStr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class ImplementStrStr
+	{
+		public static int StrStr1(string haystack, string needle)
+		{
+			if (string.IsNullOrWhiteSpace(needle))
+				return 0;
+
+			if (haystack == needle)
+				return 0;
+
+			int length = needle.Length;
+
+			for (int i = 0; i <= haystack.Length - length; i++)
+			{
+				string aux = haystack.Substring(i, length);
+				if (aux == needle)
+					return i;
+			}
+
+			return -1;
+		}
+	}
+}
