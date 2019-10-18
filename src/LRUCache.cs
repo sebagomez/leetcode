69d080dd468754e599b880f04b7c8707b23a6a7a
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-17 21:06:17 -0300

    LRU Cache implemented

---
+++ src/LRUCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+	public class LRUCache
+	{
+		readonly Dictionary<int, int> m_dict = new Dictionary<int, int>();
+		readonly int m_capacity = 0;
+		readonly LinkedList<int> m_cache;
+
+		public LRUCache(int capacity)
+		{
+			m_capacity = capacity;
+			m_cache = new LinkedList<int>();
+		}
+
+		public int Get(int key)
+		{
+			if (m_dict.ContainsKey(key))
+			{
+				if (m_cache.Contains(key))
+					m_cache.Remove(key);
+				m_cache.AddLast(key);
+				return m_dict[key];
+			}
+				
+			return -1;
+		}
+
+		public void Put(int key, int value)
+		{
+			if (m_dict.Count == m_capacity && !m_dict.ContainsKey(key))
+			{
+				int old = m_cache.First.Value;
+				m_cache.Remove(old);
+				m_dict.Remove(old);
+			}
+
+			if (m_cache.Contains(key))
+				m_cache.Remove(key);
+			m_cache.AddLast(key);
+
+			m_dict[key] = value;
+		}
+	}
+}
---
+++ tests/LRUCacheTests.cs
@@ -0,0 +1,41 @@
+﻿using Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetcodeTests
+{
+	public class LRUCacheTests
+	{
+		[Fact]
+		public void LRUCacheTests1()
+		{
+			LRUCache cache = new LRUCache(2 /* capacity */ );
+
+			cache.Put(1, 1);
+			cache.Put(2, 2);
+			Assert.Equal(1, cache.Get(1));      // returns 1
+			cache.Put(3, 3);                    // evicts key 2
+			Assert.Equal(-1, cache.Get(2));     // returns -1 (not found)
+			cache.Put(4, 4);                    // evicts key 1
+			Assert.Equal(-1, cache.Get(1));     // returns -1 (not found)
+			Assert.Equal(3, cache.Get(3));      // returns 3
+			Assert.Equal(4, cache.Get(4));      // returns 4
+		}
+
+		[Fact]
+		public void LRUCacheTests2()
+		{
+			LRUCache cache = new LRUCache(2 /* capacity */ );
+
+			Assert.Equal(-1, cache.Get(2));      
+			cache.Put(2, 6);
+			Assert.Equal(-1, cache.Get(1));
+			cache.Put(1, 5);
+			cache.Put(1, 2);
+			Assert.Equal(2, cache.Get(1));
+			Assert.Equal(6, cache.Get(2));
+		}
+	}
+}
