2ee0c2ec77852e3c915406f75ecf3cf8ff6185bd
seba gomez <sebagomezcorrea@outlook.com>
2019-10-23 13:31:07 -0300

    v0 of the K smallest

--- src/KSmallestElementSortedMatrix.cs
+++ src/KSmallestElementSortedMatrix.cs
@@ -8,7 +8,38 @@ namespace Leetcode
 	{
 		public static int KthSmallest(int[][] matrix, int k)
 		{
-			return k;
+			Dictionary<int, int[]> dict = new Dictionary<int, int[]>();
+			Dictionary<int, int> pointer = new Dictionary<int, int>();
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				dict[i] = matrix[i];
+				pointer[i] = 0;
+			}
+
+			int min = int.MaxValue;
+			int index = 0;
+			int loop = 1;
+
+			while (loop <=k)
+			{
+				int curentMin = int.MaxValue;
+				for (int i = 0; i < dict.Keys.Count; i++)
+				{
+					int p = pointer[i];
+					if (p < dict[i].Length && dict[i][p] <= curentMin)
+					{
+						index = i;
+						curentMin = dict[i][p];
+					}
+				}
+
+				min = dict[index][pointer[index]];
+				pointer[index]++;
+
+				loop++;
+			}
+
+			return min;
 		}
 	}
 }
