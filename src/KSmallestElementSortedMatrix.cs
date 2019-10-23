8c2812f9606f63d18b02281fc4ec5ae315ed5f35
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-23 11:55:58 -0300

    shell for KSmallestElement

---
+++ src/KSmallestElementSortedMatrix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+	public class KSmallestElementSortedMatrix
+	{
+		public static int KthSmallest(int[][] matrix, int k)
+		{
+			return k;
+		}
+	}
+}
---
+++ tests/KSmallestElementSortedMatrixTests.cs
@@ -0,0 +1,26 @@
+﻿using Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetcodeTests
+{
+	public class KSmallestElementSortedMatrixTests
+	{
+		[Fact]
+		public void KSmallestElementSortedMatrixTests1()
+		{
+			int[][] matrix = new int[][] { new int[] {  1,  5,  9 },
+											new int[] { 10, 11, 13 },
+											new int[] { 12, 13, 15 }};
+			InternalTest(matrix, 8, 13);
+		}
+
+		void InternalTest(int[][] matrix, int k, int expected)
+		{
+			int actual = KSmallestElementSortedMatrix.KthSmallest(matrix, k);
+			Assert.Equal(expected, actual);
+		}
+	}
+}
