310b03a929934400a4401a0c9a6dd10c567b5eae
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-30 12:01:10 -0300

    Median array

---
+++ src/MedianTwoSortedArrays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+	public class MedianTwoSortedArrays
+	{
+		public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+		{
+			int length = nums1.Length + nums2.Length;
+			int[] merge = new int[length];
+			int j = 0;
+			int k = 0;
+			for (int i = 0; i < merge.Length; i++)
+			{
+				bool take1 = (j < nums1.Length && k < nums2.Length && nums1[j] <= nums2[k]) || 
+							((j == nums1.Length || k == nums2.Length) && j < nums1.Length);
+				if (take1)
+				{
+					merge[i] = nums1[j];
+					j++;
+				}
+				else
+				{
+					merge[i] = nums2[k];
+					k++;
+				}
+			}
+
+			if (length % 2 == 0)
+				return (merge[(length / 2) - 1] + merge[length / 2]) / 2d;
+			else
+				return merge[length / 2];
+		}
+	}
+}
---
+++ tests/MedianTwoSortedArraysTests.cs
@@ -0,0 +1,41 @@
+﻿using Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetcodeTests
+{
+	public class MedianTwoSortedArraysTests
+	{
+		[Fact]
+		public void MedianTwoSortedArraysTests1()
+		{
+			int[] nums1 = new int[] { 1, 3 };
+			int[] nums2 = new int[] { 2 };
+			InternalTest(nums1, nums2, 2.0);
+		}
+
+		[Fact]
+		public void MedianTwoSortedArraysTests2()
+		{
+			int[] nums1 = new int[] { 1, 2 };
+			int[] nums2 = new int[] { 3, 4 };
+			InternalTest(nums1, nums2, 2.5);
+		}
+
+		[Fact]
+		public void MedianTwoSortedArraysTests3()
+		{
+			int[] nums1 = new int[] { 3 };
+			int[] nums2 = new int[] { -2, -1 };
+			InternalTest(nums1, nums2, -1);
+		}
+
+		void InternalTest(int[] nums1, int[] nums2, double expected)
+		{
+			double actual = MedianTwoSortedArrays.FindMedianSortedArrays(nums1, nums2);
+			Assert.Equal(expected, actual);
+		}
+	}
+}
