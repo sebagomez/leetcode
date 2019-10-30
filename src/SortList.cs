795dc8d6372d4defaf3a332e70e962b6d2dcd5a3
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-30 10:59:39 -0300

    Sort List

---
+++ src/SortList.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+	public class SortList
+	{
+		public static ListNode Solve(ListNode head)
+		{
+			List<ListNode> aux = new List<ListNode>();
+			while (head != null)
+			{
+				aux.Add(head);
+				head = head.next;
+			}
+
+			ListNode root = null;
+			foreach (ListNode node in aux)
+			{
+				node.next = null;
+				if (root == null)
+				{
+					root = node;
+					continue;
+				}
+
+				if (node.val <= root.val)
+				{
+					node.next = root;
+					root = node;
+				}
+				else
+				{
+					ListNode index = root;
+					while (index.next != null && node.val > index.next.val)
+						index = index.next;
+
+					ListNode tmp = index.next;
+					index.next = node;
+					node.next = tmp;
+				}
+
+			}
+
+			return root;
+		}
+	}
+}
---
+++ tests/SortListTests.cs
@@ -0,0 +1,35 @@
+﻿using Leetcode;
+using LeetCode.Helpers;
+using LeetcodeTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetcodeTests
+{
+	public class SortListTests
+	{
+		[Fact]
+		public void SortListTests1()
+		{
+			ListNode head = ListNode.FromArray(new int[] { 4, 2, 1, 3 });
+			ListNode expected = ListNode.FromArray(new int[] { 1, 2, 3, 4 });
+			InternalTest(head, expected);
+		}
+
+		[Fact]
+		public void SortListTests2()
+		{
+			ListNode head = ListNode.FromArray(new int[] { -1, 5, 3, 4, 0 });
+			ListNode expected = ListNode.FromArray(new int[] { -1, 0, 3, 4, 5 });
+			InternalTest(head, expected);
+		}
+
+		void InternalTest(ListNode head, ListNode expected)
+		{
+			ListNode actual = SortList.Solve(head);
+			AssertHelper.AssertListNode(expected, actual);
+		}
+	}
+}
