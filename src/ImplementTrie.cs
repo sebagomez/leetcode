4bd5f3c3f371863ee721aab56213bb4f11014959
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-30 14:01:18 -0300

    Trie implementation

---
+++ src/ImplementTrie.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+	public class ImplementTrie
+	{
+		public ImplementTrieNode Root { get; set; }
+
+		public ImplementTrie()
+		{
+			Root = new ImplementTrieNode();
+		}
+
+		/** Inserts a word into the trie. */
+		public void Insert(string word)
+		{
+			ImplementTrieNode current = Root;
+			for (int i = 0; i < word.Length; i++)
+			{
+				char c = word[i];
+				if (!current.Next.ContainsKey(c))
+					current.Next[c] = new ImplementTrieNode();
+				
+				current = current.Next[c];
+			}
+
+			current.Final = true;
+		}
+
+		/** Returns if the word is in the trie. */
+		public bool Search(string word)
+		{
+			ImplementTrieNode current = Root;
+			for (int i = 0; i < word.Length; i++)
+			{
+				char c = word[i];
+				if (!current.Next.ContainsKey(c))
+					return false;
+
+				current = current.Next[c];
+			}
+
+			return current.Final;
+		}
+
+		/** Returns if there is any word in the trie that starts with the given prefix. */
+		public bool StartsWith(string prefix)
+		{
+			ImplementTrieNode current = Root;
+			for (int i = 0; i < prefix.Length; i++)
+			{
+				char c = prefix[i];
+				if (!current.Next.ContainsKey(c))
+					return false;
+
+				current = current.Next[c];
+			}
+
+			return true;
+		}
+
+
+	}
+
+	public class ImplementTrieNode
+	{
+		public Dictionary<char,ImplementTrieNode> Next { get; set; }
+		public bool Final { get; set; }
+
+		public ImplementTrieNode()
+		{
+			Next = new Dictionary<char, ImplementTrieNode>();
+		}
+	}
+}
---
+++ tests/ImplementTrieTests.cs
@@ -0,0 +1,28 @@
+﻿using Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetcodeTests
+{
+	public class ImplementTrieTests
+	{
+		[Fact]
+		public void ImplementTrieTests1()
+		{
+			ImplementTrie trie = new ImplementTrie();
+
+			trie.Insert("apple");
+			bool result = trie.Search("apple");   // returns true
+			Assert.True(result);
+			result = trie.Search("app");     // returns false
+			Assert.False(result);
+			result = trie.StartsWith("app"); // returns true
+			Assert.True(result);
+			trie.Insert("app");
+			result = trie.Search("app");     // returns true
+			Assert.True(result);
+		}
+	}
+}
