bc8637216f8f9145d3584981ead5412e5fc9ef05
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-11-03 16:04:10 -0300

    Improved (little bit) LCA

--- src/LowestCommonAncestorBinaryTree.cs
+++ src/LowestCommonAncestorBinaryTree.cs
@@ -9,6 +9,28 @@ namespace Leetcode
 	{
 		public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
 		{
+			return FindLCARecursive(root, p, q);
+		}
+
+		private static TreeNode FindLCARecursive(TreeNode root, TreeNode p, TreeNode q)
+		{
+			if (root == null)
+				return null;
+
+			TreeNode left = FindLCARecursive(root.left, p, q);
+			TreeNode right = FindLCARecursive(root.right, p, q);
+
+			if (left != null && right != null)
+				return root;
+
+			if (root.val == q.val || root.val == p.val)
+				return root;
+
+			return left != null ? left : right;
+		}
+
+		public static TreeNode LowestCommonAncestor_solw(TreeNode root, TreeNode p, TreeNode q)
+		{
 			List<TreeNode> listP = new List<TreeNode>();
 			List<TreeNode> listQ = new List<TreeNode>();
 			GetPathList(root, p, listP);
