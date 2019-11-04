cc509f036f44d477832bf68e18a80d136fb9895c
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-11-03 23:51:06 -0300

    Sudoku solver helped by @bephrem1

---
+++ src/SudokuSolver.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+	public class SudokuSolver
+	{
+		const char EMPTY = '.';
+
+		public static void SolveSudoku(char[][] board)
+		{
+			SolveCell(board, 0, 0);
+		}
+
+		private static bool SolveCell(char[][] board, int row, int col)
+		{
+			if (col == board[row].Length)
+			{
+				col = 0;
+				row++;
+
+				if (row == board.Length)
+					return true;
+			}
+
+			if (board[row][col] != EMPTY)
+				return SolveCell(board, row, col + 1);
+
+			for (int i = 1; i <= 9; i++)
+			{
+				char c = (char)(i + '0');
+				if (IsValidCell(board, row, col, c))
+				{
+					board[row][col] = c;
+					if (SolveCell(board, row, col + 1))
+						return true;
+					else
+						board[row][col] = EMPTY;
+				}
+			}
+
+			return false;
+		}
+
+		private static bool IsValidCell(char[][] board, int row, int col, char c)
+		{
+			// check for the row
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				if (board[row][i] == c)
+					return false;
+			}
+
+			//check for the column
+			for (int i = 0; i < board[row].GetLength(0); i++)
+			{
+				if (board[i][col] == c)
+					return false;
+			}
+
+			// check for the 3x3 subgrid
+			int firstRow = row < 3 ? 0 : row < 6 ? 3 : 6;
+			int firstCol = col < 3 ? 0 : col < 6 ? 3 : 6;
+
+			for (int i = firstRow; i < (firstRow + 3); i++)
+			{
+				for (int j = firstCol; j < (firstCol + 3); j++)
+				{
+					if (board[i][j] == c)
+						return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ tests/SudokuSolverTests.cs
@@ -0,0 +1,44 @@
+﻿using Leetcode;
+using LeetcodeTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetcodeTests
+{
+	public class SudokuSolverTests
+	{
+		[Fact]
+		public void SudokuSolverTests1()
+		{
+			char[][] board = new char[][] { new char[]{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+											new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+											new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+											new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+											new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+											new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+											new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+											new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+											new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+			char[][] expected = new char[][] { new char[]{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
+											   new char[]{'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+											   new char[]{'1', '9', '8', '3', '4', '2', '5', '6', '7'},
+											   new char[]{'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+											   new char[]{'4', '2', '6', '8', '5', '3', '7', '9', '1'},
+											   new char[]{'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+											   new char[]{'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+											   new char[]{'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+											   new char[]{'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
+
+			InternalTest(board, expected);
+		}
+
+		void InternalTest(char[][] board, char[][] expected)
+		{
+			SudokuSolver.SolveSudoku(board);
+			AssertHelper.AssertMatrixes(expected, board);
+		}
+	}
+}
