090b8bd14f03c99f05513397cb99f37ad518b2b7
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-11-12 10:23:57 -0300

    Generate Parentheses

---
+++ src/GenerateParentheses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+	public class GenerateParentheses
+	{
+		const string OPEN = "(";
+		const string CLOSE = ")";
+
+		public static IList<string> GenerateParenthesis(int n)
+		{
+			List<string> list = new List<string>();
+
+			if (n == 0)
+				return list;
+
+			GetSet(n, n, "", list);
+
+			return list;
+		}
+
+		private static void GetSet(int left, int right, string subSolution, List<string> result)
+		{
+			// base case
+			if (left == 0 && right == 0)
+			{
+				result.Add(subSolution);
+				return;
+			}
+			// recursive rule
+			if (left > 0)
+			{
+				GetSet(left - 1, right, subSolution + "(", result);
+			}
+			if (right > left)
+			{
+				GetSet(left, right - 1, subSolution + ")", result);
+			}
+		}
+	}
+}
+
---
+++ tests/GenerateParenthesesTests.cs
@@ -0,0 +1,36 @@
+﻿using Leetcode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetcodeTests
+{
+	public class GenerateParenthesesTests
+	{
+		[Fact]
+		public void GenerateParenthesesTests1()
+		{
+			List<string> expected = new List<string>
+			{
+				"((()))",
+				"(()())",
+				"(())()",
+				"()(())",
+				"()()()"
+			};
+
+			InternalTest(3, expected);
+		}
+
+		void InternalTest(int n, IList<string> expected)
+		{
+			IList<string> actual = GenerateParentheses.GenerateParenthesis(n);
+			Assert.Equal(expected.Count, actual.Count);
+			for (int i = 0; i < actual.Count; i++)
+			{
+				Assert.Equal(expected[i], actual[i]);
+			}
+		}
+	}
+}
